08b1d35ad535196fb2be4781d756e660f4c17f0e
sohamkamani <sohamkamani@gmail.com>
2016-03-28 01:23:35 +0530

    fancified

--- src/index.js
+++ src/index.js
@@ -8,28 +8,42 @@ ctx.fillStyle = '#000';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 const dimensions = {
-  w : 100,
-  h : 80
+  w: 100,
+  h: 80
 };
-const img = new Image(dimensions.w, dimensions.h);
-img.onload = function () {
+let img;
+const onImageLoad = function () {
 
-    var oc = document.createElement('canvas'),
-        octx = oc.getContext('2d');
-    oc.width = dimensions.w;
-    oc.height = dimensions.h;
-    octx.drawImage(img, 0, 0, oc.width, oc.height);
+  var oc = document.createElement('canvas'),
+    octx = oc.getContext('2d');
+  oc.width = dimensions.w;
+  oc.height = dimensions.h;
+  octx.drawImage(img, 0, 0, oc.width, oc.height);
 
-    const imageData = octx.getImageData(0,0,oc.width, oc.height);
-    const imgArray = Array.from(imageData.data);
-    const renderer = createRenderer({
-      arr : imgArray,
-      ctx,
-      width : canvas.width,
-      height : canvas.height,
-      dimensions
-    });
+  const imageData = octx.getImageData(0, 0, oc.width, oc.height);
+  const imgArray = Array.from(imageData.data);
+  const renderer = createRenderer({
+    arr: imgArray,
+    ctx,
+    width: canvas.width,
+    height: canvas.height,
+    dimensions
+  });
 
-    setInterval(()=>renderer.render(), 1000 / 30);
+  setInterval(() => renderer.render(), 1000 / 30);
 };
-img.src = '/images/mountain.jpg';
+
+// img.src = '/images/mountain.jpg';
+
+var imageLoader = document.getElementById('imageLoader');
+imageLoader.addEventListener('change', handleImage, false);
+
+function handleImage(e) {
+  var reader = new FileReader();
+  reader.onload = function (event) {
+    img = new Image(dimensions.w, dimensions.h);
+    img.onload = onImageLoad;
+    img.src = event.target.result;
+  };
+  reader.readAsDataURL(e.target.files[0]);
+}
